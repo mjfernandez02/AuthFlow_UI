c1eea6ca29365d135c39971d3eb63204b437ff77
mjfernandez02 <fmikejordan1213@gmail.com>
2026-09-13 23:46:05 +0800

    chore: remove legacy security dashboard modules

--- src/dashboard/index.jsx
+++
@@ -1,6 +0,0 @@
-import Overview from "./Overview";
-import SecurityCenter from "./SecurityCenter";
-import ApiKeys from "./ApiKeys";
-import Sessions from "./Sessions";
-
-export { Overview, SecurityCenter, ApiKeys, Sessions };
