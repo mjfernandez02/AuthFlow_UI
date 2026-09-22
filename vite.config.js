7fdb412f6d5ee0cf583cfc3329ed59caa3aa4009
mjfernandez02 <fmikejordan1213@gmail.com>
2026-09-23 04:51:08 +0800

    create vendor file to cache and separate used dependencies from app files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   envPrefix: ["VITE_", "REACT_APP_"],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor",
+              test: /node_modules/,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
